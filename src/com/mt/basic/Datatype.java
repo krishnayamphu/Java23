7427fe8ad4906ad74a54bd5e25dab1cbe47b2c37
KrishnaYamphu <krishnayamphu@gmail.com>
2022-07-31 14:52:44 +0545

    java basics

---
+++ src/com/mt/basic/Datatype.java
@@ -0,0 +1,19 @@
+package com.mt.basic;
+
+public class Datatype {
+    public static void main(String[] args) {
+        int a=5;
+        short b=10;
+        float f=5.5f;
+        double d=4.5d;
+        char ch='a';
+        boolean bool=true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(bool);
+    }
+}
---
+++ src/com/mt/basic/Test.java
@@ -0,0 +1,21 @@
+package com.mt.basic;
+
+public class Test {
+    public static void main(String[] args) {
+        int x=5, y=6, sum=0;
+        sum=x+y;
+        System.out.println("Total sum: "+sum);
+
+        char v='a';
+        System.out.println(v);
+
+        String name="Aptech";
+        int age=10;
+
+        System.out.println("Name: "+name+", Age: "+age);
+
+        for(int i=1; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
